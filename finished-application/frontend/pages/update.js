fc8aa48d9d9e6d86ab9490ca350e18781a7aaa3e
Lucio <luciobertinchavez@gmail.com>
2021-05-20 17:11:46 -0500

    use ClientOnly to wrap UpdateProduct

--- finished-application/frontend/pages/update.js
+++ finished-application/frontend/pages/update.js
@@ -1,10 +1,13 @@
 import UpdateProduct from '../components/UpdateProduct';
+import ClientOnly from '../components/ClientOnly';
 
 export default function UpdatePage({ query }) {
   console.log(query);
   return (
     <div>
-      <UpdateProduct id={query.id} />
+      <ClientOnly>
+        <UpdateProduct id={query.id} />
+      </ClientOnly>
     </div>
   );
 }
